44d1febdedd981b7154d14c49abd5eafcf06f030
eatmynerds <vipershniper07@gmail.com>
2024-12-09 19:07:14 -0600

    Update

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,17 @@ extern crate imlib_rs;
 
 use clap::Parser;
 use core::mem::{align_of, size_of};
+use display_info::DisplayInfo;
 use env_logger::Env;
 use log::info;
 use std::{ffi::CString, fs, path::Path, time::Duration};
 use x11::xlib::{Pixmap, Window};
 
 const MICROSECONDS_PER_SECOND: u64 = 1_000_000;
-const TARGET_FPS: u64 = 60;
 
 #[derive(Clone, Copy, Debug)]
 struct Monitor {
+    refresh_rate: f32,
     root: Window,
     pixmap: Pixmap,
     width: usize,
@@ -43,20 +44,20 @@ fn safe_ptr_cast<A, B>(a: *mut A) -> *mut B {
     a.cast()
 }
 
-unsafe fn set_root_atoms(display: *mut x11::xlib::_XDisplay, monitor: &Monitor) {
-    let atom_root: x11::xlib::Atom = x11::xlib::XInternAtom(
+unsafe fn set_root_atoms(display: *mut x11::xlib::_XDisplay, monitor: Monitor) {
+    let atom_root = x11::xlib::XInternAtom(
         display,
         CString::new("_XROOTPMAP_ID").unwrap().as_ptr() as *const i8,
         false as i32,
     );
 
-    let atom_eroot: x11::xlib::Atom = x11::xlib::XInternAtom(
+    let atom_eroot = x11::xlib::XInternAtom(
         display,
         CString::new("ESETROOT_PMAP_ID").unwrap().as_ptr() as *const i8,
         false as i32,
     );
 
-    let monitor_pixmap = monitor.pixmap as u8;
+    let monitor_pixmap = monitor.pixmap as u64;
 
     x11::xlib::XChangeProperty(
         display,
@@ -65,7 +66,7 @@ unsafe fn set_root_atoms(display: *mut x11::xlib::_XDisplay, monitor: &Monitor) 
         x11::xlib::XA_PIXMAP,
         32,
         x11::xlib::PropModeReplace,
-        &monitor_pixmap as *const u8,
+        &monitor_pixmap as *const u64 as *const u8,
         1,
     );
 
@@ -76,7 +77,7 @@ unsafe fn set_root_atoms(display: *mut x11::xlib::_XDisplay, monitor: &Monitor) 
         x11::xlib::XA_PIXMAP,
         32,
         x11::xlib::PropModeReplace,
-        &monitor_pixmap as *const u8,
+        &monitor_pixmap as *const u64 as *const u8,
         1,
     );
 }
@@ -90,7 +91,9 @@ unsafe fn get_monitors() -> (*mut x11::xlib::_XDisplay, Vec<Monitor>) {
 
     let mut monitors: Vec<Monitor> = Vec::with_capacity(screen_count as usize);
 
-    for current_screen in 0..=screen_count - 1 {
+    let display_infos = DisplayInfo::all().unwrap();
+
+    for (current_screen, display_info) in (0..screen_count).zip(display_infos) {
         info!("Running screen {}", current_screen);
 
         let width = x11::xlib::XDisplayWidth(display, current_screen);
@@ -114,6 +117,7 @@ unsafe fn get_monitors() -> (*mut x11::xlib::_XDisplay, Vec<Monitor>) {
             x11::xlib::XCreatePixmap(display, root, width as u32, height as u32, depth as u32);
 
         monitors.push(Monitor {
+            refresh_rate: display_info.frequency,
             root,
             pixmap,
             width: width as usize,
@@ -137,47 +141,66 @@ unsafe fn get_monitors() -> (*mut x11::xlib::_XDisplay, Vec<Monitor>) {
 
 unsafe fn run(
     display: *mut x11::xlib::_XDisplay,
-    monitors: &[Monitor],
+    monitor: Monitor,
     current_image: imlib_rs::Imlib_Image,
 ) {
-    for monitor in monitors {
-        imlib_rs::imlib_context_push(monitor.render_context);
-        imlib_rs::imlib_context_set_dither(1);
-        imlib_rs::imlib_context_set_blend(1);
-        imlib_rs::imlib_context_set_image(current_image);
+    imlib_rs::imlib_context_push(monitor.render_context);
+    imlib_rs::imlib_context_set_dither(1);
+    imlib_rs::imlib_context_set_blend(1);
+    imlib_rs::imlib_context_set_image(current_image);
 
-        let original_width = imlib_rs::imlib_image_get_width();
-        let original_height = imlib_rs::imlib_image_get_height();
+    let original_width = imlib_rs::imlib_image_get_width();
+    let original_height = imlib_rs::imlib_image_get_height();
 
-        let scaled_image = imlib_rs::imlib_create_cropped_scaled_image(
-            0,
-            0,
-            original_width,
-            original_height,
-            monitor.width as i32,
-            monitor.height as i32,
+    let scaled_image = imlib_rs::imlib_create_cropped_scaled_image(
+        0,
+        0,
+        original_width,
+        original_height,
+        monitor.width as i32,
+        monitor.height as i32,
+    );
+
+    imlib_rs::imlib_context_set_image(scaled_image);
+    imlib_rs::imlib_render_image_on_drawable(0, 0);
+
+    set_root_atoms(display, monitor);
+
+    // ----
+
+    x11::xlib::XSetCloseDownMode(display, x11::xlib::RetainTemporary);
+    x11::xlib::XKillClient(display, x11::xlib::AllTemporary as u64);
+    x11::xlib::XSetWindowBackgroundPixmap(display, monitor.root, monitor.pixmap);
+    x11::xlib::XClearWindow(display, monitor.root);
+    x11::xlib::XFlush(display);
+    x11::xlib::XSync(display, false as i32);
+
+    imlib_rs::imlib_free_image_and_decache();
+}
+
+unsafe fn render(
+    display: *mut x11::xlib::_XDisplay,
+    monitor: Monitor,
+    images: Vec<imlib_rs::Imlib_Image>,
+    images_count: usize,
+) {
+    let mut cycle = 0;
+
+    loop {
+        cycle += 1;
+        let current: imlib_rs::Imlib_Image = images[cycle % images_count];
+
+        run(display, monitor, current);
+
+        let timeout = Duration::from_nanos(
+            (MICROSECONDS_PER_SECOND / monitor.refresh_rate as u64) * 1_000, // nanoseconds
         );
 
-        imlib_rs::imlib_context_set_image(scaled_image);
-        imlib_rs::imlib_render_image_on_drawable(0, 0);
-
-        set_root_atoms(display, monitor);
-
-        // ----
-
-        x11::xlib::XKillClient(display, x11::xlib::AllTemporary as u64);
-        x11::xlib::XSetCloseDownMode(display, x11::xlib::RetainTemporary);
-        x11::xlib::XSetWindowBackgroundPixmap(display, monitor.root, monitor.pixmap);
-        x11::xlib::XClearWindow(display, monitor.root);
-        x11::xlib::XFlush(display);
-        x11::xlib::XSync(display, false as i32);
-
-        imlib_rs::imlib_free_image_and_decache();
-        // ----
+        std::thread::sleep(timeout);
     }
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or("debug")).init();
 
     let args = CliImagePath::parse();
@@ -211,21 +234,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         info!("Starting the program...");
 
-        let mut cycle = 0;
-
-        loop {
-            cycle += 1;
-            let current: imlib_rs::Imlib_Image = images[cycle % images_count];
-
-            /* TODO: Figure out why it does not render with picom
-            running while the C version does */
-            run(display, &monitors.clone(), current);
-
-            let timeout = Duration::from_nanos(
-                (MICROSECONDS_PER_SECOND / TARGET_FPS) * 1_000, // nanoseconds
-            );
-
-            std::thread::sleep(timeout);
+        for monitor in monitors {
+            render(display, monitor, images.clone(), images_count);
         }
     }
 }
