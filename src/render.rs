02669217da8d5a5814c54d01ff9c4cfa2051ef67
Jason Spalti <vipershniper08@gmail.com>
2024-12-17 05:36:02 -0600

    Update render.rs

--- src/render.rs
+++ src/render.rs
@@ -55,10 +55,10 @@ unsafe fn composite_images(
     info!("Creating bitmap output directory");
 
     if std::fs::exists("output-bmps").expect("Failed to check if output bitmap directory exists!") {
-        std::fs::remove_dir_all("output-bmps").expect("Failed to create output bitmap directory!");
+        std::fs::remove_dir_all("output-bmps").expect("Failed to remove output bitmap directory!");
     }
 
-    std::fs::create_dir("output-bmps").expect("Failed to remove output bitmap directory!");
+    std::fs::create_dir("output-bmps").expect("Failed to create output bitmap directory!");
 
     info!("Compositing bitmap images");
     ImlibContextPush(monitor.render_context);
